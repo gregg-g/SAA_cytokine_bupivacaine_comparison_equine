7531ec1459500708b8179a90d0943a21cdd42f6a
gregg-g <gregg_griffenhagen@me.com>
2025-10-02 13:50:57 -0600

    Initial commit

---
+++ graphics.R
@@ -0,0 +1,275 @@
+library(ggrepel)
+  # add separator for new controls and older data
+data$time <- ifelse(data$ID == 'Vesunna'|
+                      data$ID == 'Rita'|
+                      data$ID == 'Myla',
+                                      'new', 'old') %>% factor()
+str(data)  
+  #test plot
+labels1 <- data %>% filter(time == 'new' & hrs == 72)
+labels2 <- data %>% filter(ID == 'Rita' & hrs == 48)
+  # create separately d/t Rita data ending at 48 hrs
+
+p_saa <- ggplot(data, aes(hrs, saa)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+print(p_saa)
+
+p <- ggplot(data, aes(hrs, Eotaxin)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+# works as expected
+p2 <- ggplot(data, aes(hrs, G.CSF)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p3 <- ggplot(data, aes(hrs, IL1a)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p4 <- ggplot(data, aes(hrs, IL.5)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p5 <- ggplot(data, aes(hrs, IL.18)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p6 <- ggplot(data, aes(hrs, IL.6)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p7 <- ggplot(data, aes(hrs, IL.17A)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p8 <- ggplot(data, aes(hrs, IL.2)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p9 <- ggplot(data, aes(hrs, IL.4)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p10 <- ggplot(data, aes(hrs, IFNg)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p11 <- ggplot(data, aes(hrs, IL.8)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p12 <- ggplot(data, aes(hrs, IP.10)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p13 <- ggplot(data, aes(hrs, IL.10)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+p14 <- ggplot(data, aes(hrs, TNFa)) +
+  geom_line(aes(group = ID, color = time)) +
+  geom_text_repel(data = labels1, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3) +
+  geom_text_repel(data = labels2, aes(label = ID),
+                  fontface ="plain", color = "black", size = 3)
+
+plot_list <- list(p, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14)
+#cols_to_plot <- c("Eotaxin", "G.CSF", "IL1a", "IL.5", "IL.18",
+#                  "IL.6", "IL.17A", "IL.2", "IL.4", "IFNg",
+#                  "IL.8", "IP.10", "IL.10", "TNFa")
+#plot_list <- list()
+#for (col_name in cols_to_plot) {
+#                             p <- ggplot(data, aes_string(x=data$hrs, y=col_name)) +
+#                               geom_line(aes(color=time, group = ID)) +
+#                             geom_text_repel(data = labels1, aes(label = ID),
+#                                                 fontface ="plain", color = "black", size = 3) +
+#                               geom_text_repel(data = labels2, aes(label = ID),
+#                                               fontface ="plain", color = "black", size = 3)
+#                             plot_list[[col_name]] <- p
+#                             print(p)
+#                           }
+
+combined_plot <- patchwork::wrap_plots(plot_list, nrow=4, guides='collect')
+print(combined_plot)
+
+#check by group
+plot_list2 <- list()
+for (col_name in cols_to_plot) {
+  p <- ggplot(data, aes_string(x=data$hrs, y=col_name)) +
+    geom_line(aes(color=group, group = ID))
+  plot_list2[[col_name]] <- p
+}
+
+combined_plot2 <- patchwork::wrap_plots(plot_list2, nrow=4, guides='collect')
+print(combined_plot2)
+
+#plot additional controls without 2 outliers (AB and Myla)
+plot_list3 <- list()
+for (col_name in cols_to_plot) {
+  p <- ggplot(data2, aes_string(x=data2$hrs, y=col_name)) +
+    geom_point(aes(color=time)) +
+    geom_line(aes(color=time, group = ID))
+  plot_list3[[col_name]] <- p
+}
+
+combined_plot3 <- patchwork::wrap_plots(plot_list3, nrow=4, guides='collect')
+print(combined_plot3)
+
+# plot all saa data
+plot1 <- ggplot(data, aes(hrs, saa))
+plot1 + geom_jitter(aes(color = group)) +
+  geom_smooth(aes(color = group), se = FALSE) +
+  geom_line(aes(group = ID))
+
+# plot saa without horse AB and Myla as outliers
+plot2 <- data %>% filter(ID != 'AB' & ID != 'Myla') %>% ggplot(aes(hrs, saa))
+plot2 + geom_jitter(aes(color = group)) +
+  geom_smooth(aes(color = group), se = FALSE) +
+  geom_line(aes(group = ID))
+
+# Better plot to evaluate groups
+# summarize data
+saa_plot_data <- data %>%
+  filter(ID != 'AB' & ID != 'Myla') %>%
+  summarySE(measurevar = 'saa', groupvars = c('group', 'hrs'))
+# set jitter
+pd <- position_dodge(3)
+# plot saa
+saa_plot <- ggplot(data = saa_plot_data, aes(x = hrs, y = saa, group = group)) +
+  geom_errorbar(aes(ymin=saa-ci, ymax=saa+ci), width=2, position=pd) +
+  geom_line(aes(linetype = group), position=pd) +
+  geom_point(aes(shape = group), size = 3, position=pd) +
+  annotate(geom = 'text', x = c(48, 72), y = c(2450, 2350), label = '*', size = 10)
+print(saa_plot)
+
+# Eotaxin plot
+eotaxin_plot_data <- data %>%
+  filter(ID != 'AB' & ID != 'Myla') %>%
+  summarySE(measurevar = 'Eotaxin', groupvars = c('group', 'hrs'))
+# set jitter
+pd <- position_dodge(3)
+# plot
+eotaxin_plot <- ggplot(data = eotaxin_plot_data, aes(x = hrs, y = Eotaxin, group = group)) +
+  geom_errorbar(aes(ymin=Eotaxin-ci, ymax=Eotaxin+ci), width=2, position=pd) +
+  geom_line(aes(linetype = group), position=pd) +
+  geom_point(aes(shape = group), size = 3, position=pd) 
+  #annotate(geom = 'text', x = c(48, 72), y = c(2450, 2350), label = '*', size = 10)
+print(eotaxin_plot)
+
+
+gcsf_plot_data <- data %>%
+  filter(ID != 'AB' & ID != 'Myla') %>%
+  summarySE(measurevar = 'G.CSF', groupvars = c('group', 'hrs'))
+# set jitter
+pd <- position_dodge(3)
+# plot
+gcsf_plot <- ggplot(data = gcsf_plot_data, aes(x = hrs, y = G.CSF, group = group)) +
+  geom_errorbar(aes(ymin=G.CSF-ci, ymax=G.CSF+ci), width=2, position=pd) +
+  geom_line(aes(linetype = group), position=pd) +
+  geom_point(aes(shape = group), size = 3, position=pd)
+  #annotate(geom = 'text', x = c(48, 72), y = c(2450, 2350), label = '*', size = 10)
+print(gcsf_plot)
+
+
+IL18_plot_data <- data %>%
+  filter(ID != 'AB' & ID != 'Myla') %>%
+  summarySE(measurevar = 'IL.18', groupvars = c('group', 'hrs'))
+# set jitter
+pd <- position_dodge(3)
+# plot
+IL18_plot <- ggplot(data = IL18_plot_data, aes(x = hrs, y = IL.18, group = group)) +
+  geom_errorbar(aes(ymin=IL.18-ci, ymax=IL.18+ci), width=2, position=pd) +
+  geom_line(aes(linetype = group), position=pd) +
+  geom_point(aes(shape = group), size = 3, position=pd) +
+  annotate(geom = 'text', x = 72, y = 280, label = '*', size = 10)
+#print(IL18_plot)
+IL18_raw <- ggplot(data2, aes(hrs, IL.18))
+IL18_raw_plot <- IL18_raw + geom_jitter(aes(color=group)) +
+  geom_smooth(aes(color=group), se=FALSE) +
+  geom_line(aes(group=ID))
+
+IL6_plot_data <- data %>%
+  filter(ID != 'AB' & ID != 'Myla') %>%
+  summarySE(measurevar = 'IL.6', groupvars = c('group', 'hrs'))
+# set jitter
+pd <- position_dodge(3)
+# plot
+IL6_plot <- ggplot(data = IL6_plot_data, aes(x = hrs, y = IL.6, group = group)) +
+  geom_errorbar(aes(ymin=IL.6-se, ymax=IL.6+se), width=2, position=pd) +
+  geom_line(aes(linetype = group), position=pd) +
+  geom_point(aes(shape = group), size = 3, position=pd) +
+  annotate(geom = 'text', x = 72, y = 125, label = '*', size = 10)
+#print(IL6_plot)
+IL6_raw <- ggplot(data2, aes(hrs, IL.6))
+IL6_raw_plot <- IL6_raw + geom_jitter(aes(color=group)) +
+  geom_smooth(aes(color=group), se=FALSE) +
+  geom_line(aes(group=ID))
+
+IL2_plot_data <- data %>%
+  filter(ID != 'AB' & ID != 'Myla') %>%
+  summarySE(measurevar = 'IL.2', groupvars = c('group', 'hrs'))
+# set jitter
+pd <- position_dodge(3)
+# plot
+IL2_plot <- ggplot(data = IL2_plot_data, aes(x = hrs, y = IL.2, group = group)) +
+  geom_errorbar(aes(ymin=IL.2-se, ymax=IL.2+se), width=2, position=pd) +
+  geom_line(aes(linetype = group), position=pd) +
+  geom_point(aes(shape = group), size = 3, position=pd) +
+  annotate(geom = 'text', x = 72, y = 110, label = '*', size = 10)
+#print(IL2_plot)
+IL2_raw <- ggplot(data2, aes(hrs, IL.2))
+IL2_raw_plot <- IL2_raw + geom_jitter(aes(color=group)) +
+  geom_smooth(aes(color=group), se=FALSE) +
+  geom_line(aes(group=ID))
+
+IL.2.6.18_plot <- patchwork::wrap_plots(IL2_plot, IL6_plot, IL18_plot,
+                                        nrow=2, guides='collect')
+print(IL.2.6.18_plot)
+
+IL.2.6.18_raw_plot <- patchwork::wrap_plots(IL2_raw_plot, IL6_raw_plot, IL18_raw_plot,
+                                            nrow=2, guides='collect')
+print(IL.2.6.18_raw_plot)
+
+IL8_plot_data <- data %>%
+  filter(ID != 'AB' & ID != 'Myla') %>%
+  summarySE(measurevar = 'IL.8', groupvars = c('group', 'hrs'))
+# set jitter
+pd <- position_dodge(3)
+# plot
+IL8_plot <- ggplot(data = IL8_plot_data, aes(x = hrs, y = IL.8, group = group)) +
+  geom_errorbar(aes(ymin=IL.8-se, ymax=IL.8+se), width=2, position=pd) +
+  geom_line(aes(linetype = group), position=pd) +
+  geom_point(aes(shape = group), size = 3, position=pd) +
+  annotate(geom = 'text', x = c(0, 0.5, 1, 2, 4, 8, 24), y = c(275, 300, 325, 350, 350, 350, 350), label = '*', size = 10)
+print(IL8_plot)
---
+++ initial_exploration.R
@@ -0,0 +1,181 @@
+library(tidyverse)
+library(magrittr)
+library(lme4)
+library(lmerTest)
+library(emmeans)
+library(outliers)
+
+data <- read.table('LL_LP_SAA_cytokine_data_0925.csv', sep = ",", header = TRUE, quote = "")
+data %<>% mutate_at(c("ID", "group"), factor)
+head(data)
+str(data)
+#add separator for new controls vs older data points
+data$time <- ifelse(data$ID == 'Vesunna'|
+                      data$ID == 'Rita'|
+                      data$ID == 'Myla',
+                    'new', 'old') %>% factor()
+
+# plot all saa data
+plot1 <- ggplot(data, aes(hrs, saa))
+plot1 + geom_jitter(aes(color = group)) +
+  geom_smooth(aes(color = group), se = FALSE) +
+  geom_line(aes(group = ID))
+
+# plot saa without horse AB as outlier
+plot2 <- data %>% filter(ID != 'AB' & ID != 'Myla') %>% ggplot(aes(hrs, saa))
+plot2 + geom_jitter(aes(color = group)) +
+  geom_smooth(aes(color = group), se = FALSE) +
+  geom_line(aes(group = ID))
+
+# check time zero for initial outliers
+saa_0 <- data %>% filter (hrs == 0) %>% .$saa
+dixon.test(saa_0, two.sided=F)
+  #yes, value of 1716 at time 0 is an outlier in horse AB
+  #Check eotaxin, gcsf and IL8
+eotaxin_0 <- data %>% filter (hrs == 0) %>% .$Eotaxin
+dixon.test(eotaxin_0, two.sided = F)
+gcsf_0 <- data %>% filter (hrs == 0) %>% .$G.CSF
+dixon.test(gcsf_0, two.sided = F)
+IL8_0 <- data %>% filter (hrs == 0) %>% .$IL.8
+dixon.test(IL8_0, two.sided = F)
+  #all yes, Myla is an outlier
+
+# remove the outliers for analysis
+data2 <- data %>% filter(ID != 'AB' & ID != 'Myla')
+
+detach(package:plyr)
+data_summary <- data2 %>% 
+  group_by(hrs, group) %>%
+  summarise(mean = mean(saa), sd = sd(saa), min = min(saa), max = max(saa))
+#knitr::kable(data_summary)
+kbl(data_summary, longtable = T, booktabs = T, align = "c") %>%
+  column_spec(1, bold=T) %>%
+  collapse_rows(columns = 1:2, latex_hline = "major", valign = "middle")
+
+# create model
+model_small <- lmer(saa ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_small, "lmerModLmerTest"), ddf="Kenward-Roger"))
+# note the only real difference is over time, no groups are different in this analysis
+# interestingly, the slope for LB40 is different over time while the others are not
+
+# compare all 3 groups at each time point
+emm_small2 <- emmeans(model_small, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+emm_small2
+pairs(emm_small2) # there are small differences in 20 vs 40 at 48 & 72 hrs
+                  # with 40 being lower than 20
+contrast(emm_small2, "trt.vs.ctrl") # no differences now
+
+# compare all horses over time without regard for group
+model_small2 <- lmer(saa ~ factor(hrs) + (1|ID), data = data2)
+emm_small3 <- emmeans(model_small2, ~ hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+contrast(emm_small3, "trt.vs.ctrl")
+
+# Eotaxin, G-CSF, IL1a, IL-5, IL-18, IL-6, IL-17A, IL-2, IL-4, IFNg, IL-8, IP-10, IL-10, TNFa
+model_eotaxin <- lmer(Eotaxin ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_eotaxin, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_eotaxin <- emmeans(model_eotaxin, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_eotaxin)
+contrast(emm_eotaxin, "trt.vs.ctrl")
+  #lots of differences - need to graph
+
+model_gcsf <- lmer(G.CSF ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_gcsf, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_gcsf <- emmeans(model_gcsf, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_gcsf)
+contrast(emm_gcsf, "trt.vs.ctrl")
+  #differences vs control at early time points - graph
+
+model_IL5 <- lmer(IL.5 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL5, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_IL5 <- emmeans(model_IL5, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL5)
+contrast(emm_IL5, "trt.vs.ctrl")
+  #nothing
+model_IL18 <- lmer(IL.18 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL18, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_IL18 <- emmeans(model_IL18, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL18)
+contrast(emm_IL18, "trt.vs.ctrl")
+  #20 vs 40 at 72 hrs??? - likely nothing
+
+model_IL1a <- lmer(IL1a ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL1a, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there - comes close at 48 & 72
+emm_IL1a <- emmeans(model_IL1a, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL1a)
+contrast(emm_IL1a, "trt.vs.ctrl")
+  #nothing
+model_IL.6 <- lmer(IL.6 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL.6, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there - comes close at 48 & 72
+emm_IL.6 <- emmeans(model_IL.6, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL.6)
+contrast(emm_IL.6, "trt.vs.ctrl")
+  #20 vs 40 at 72 hrs??? - likely nothing, except 2nd cytokine
+
+model_IL.17A <- lmer(IL.17A ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL.17A, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_IL.17A <- emmeans(model_IL.17A, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL.17A)
+contrast(emm_IL.17A, "trt.vs.ctrl")
+  #nothing
+model_IL.2 <- lmer(IL.2 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL.2, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_IL.2 <- emmeans(model_IL.2, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL.2)
+contrast(emm_IL.2, "trt.vs.ctrl")
+#20 vs 40 at 72 hrs??? - likely nothing, except 3rd cytokine?
+
+model_IL.4 <- lmer(IL.4 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL.4, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_IL.4 <- emmeans(model_IL.4, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL.4)
+contrast(emm_IL.4, "trt.vs.ctrl")
+  #nothing
+#####
+model_IFNg <- lmer(IFNg ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IFNg, "lmerModLmerTest"), ddf="Kenward-Roger")) # 20 & 40 diff at 48 & 72
+emm_IFNg <- emmeans(model_IFNg, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IFNg)
+contrast(emm_IFNg, "trt.vs.ctrl")
+  #nothing
+model_IL.8 <- lmer(IL.8 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL.8, "lmerModLmerTest"), ddf="Kenward-Roger")) # 20 & 40 diff early on
+emm_IL.8 <- emmeans(model_IL.8, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL.8)
+contrast(emm_IL.8, "trt.vs.ctrl")
+  #LB20 vs control at early time points - argues a drug reaction??? plus 20 vs 40 is significant
+
+model_IP.10 <- lmer(IP.10 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IP.10, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_IP.10 <- emmeans(model_IP.10, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IP.10)
+contrast(emm_IP.10, "trt.vs.ctrl")
+
+model_IL.10 <- lmer(IL.10 ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_IL.10, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_IL.10 <- emmeans(model_IL.10, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_IL.10)
+contrast(emm_IL.10, "trt.vs.ctrl")
+
+model_TNFa <- lmer(TNFa ~ group*hrs + (1|ID), data = data2)
+coef(summary(as(model_TNFa, "lmerModLmerTest"), ddf="Kenward-Roger")) # Nothing there
+emm_TNFa <- emmeans(model_TNFa, ~ group|hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+pairs(emm_TNFa)
+contrast(emm_TNFa, "trt.vs.ctrl")
+
+# Check all cytokines for changes from baseline without regard for group
+
+model_IL1B_nogroup <- lmer(IL1B ~ factor(hrs) + (1|ID), data = data2)
+emm_IL1B_nogroup <- emmeans(model_IL1B_nogroup, ~ hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+contrast(emm_IL1B_nogroup, "trt.vs.ctrl")
+
+model_IFNg_nogroup <- lmer(IFNg ~ factor(hrs) + (1|ID), data = data2)
+emm_IFNg_nogroup <- emmeans(model_IFNg_nogroup, ~ hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+contrast(emm_IFNg_nogroup, "trt.vs.ctrl")
+
+model_IL.10_nogroup <- lmer(IL.10 ~ factor(hrs) + (1|ID), data = data2)
+emm_IL.10_nogroup <- emmeans(model_IL.10_nogroup, ~ hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+contrast(emm_IL.10_nogroup, "trt.vs.ctrl")
+
+model_TNFa_nogroup <- lmer(TNFa ~ factor(hrs) + (1|ID), data = data2)
+emm_TNFa_nogroup <- emmeans(model_TNFa_nogroup, ~ hrs, at = list(hrs=c(0,0.5, 1, 2, 4, 8, 24, 48, 72)))
+contrast(emm_TNFa_nogroup, "trt.vs.ctrl")
